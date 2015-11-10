3e825300515daae99abab31d6e4e9b6bfed08c97
Tim Dykes <ttdykes@Tims-MacBook-Pro.local>
2015-11-10 23:22:43 +1100

    refactor

--- Beacon Enhancement Suite (BES)/shared_job_code.js
+++ Beacon Enhancement Suite (BES)/shared_job_code.js
@@ -1,34 +1,34 @@
-function GetUnitNamefromBeacon(Id,callback) {
-    console.log("GetUnitNamefromBeacon called with:"+Id);
+function GetUnitNamefromBeacon(Id, callback) {
+    console.log("GetUnitNamefromBeacon called with:" + Id);
 
 
-var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-        results = JSON.parse(xhttp.responseText);
-        
-        if (typeof callback === "function") {
-    console.log("GetUnitNamefromBeacon call back with:"+results.Name);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            results = JSON.parse(xhttp.responseText);
 
-            callback(results.Name);
+            if (typeof callback === "function") {
+                console.log("GetUnitNamefromBeacon call back with:" + results.Name);
+
+                callback(results.Name);
+
+            }
+
 
         }
-
-
     }
-  }
-  
-  xhttp.open("GET", "https://beacon.ses.nsw.gov.au/Api/v1/Entities/"+params.hq, true);
-  xhttp.send();
+
+    xhttp.open("GET", "https://beacon.ses.nsw.gov.au/Api/v1/Entities/" + params.hq, true);
+    xhttp.send();
 
 
 }
 
 
 //make the call to beacon
-function GetJSONfromBeacon(Id,StartDate,EndDate,callback) {
+function GetJSONfromBeacon(Id, StartDate, EndDate, callback) {
 
-    console.log("GetJSONfromBeacon called with:"+Id +","+StartDate+","+EndDate);
+    console.log("GetJSONfromBeacon called with:" + Id + "," + StartDate + "," + EndDate);
 
 
     var xhttp = new XMLHttpRequest();
@@ -46,14 +46,14 @@ function GetJSONfromBeacon(Id,StartDate,EndDate,callback) {
             // Make sure the callback is a function​
             if (typeof callback === "function") {
 
-            callback(jobs);
+                callback(jobs);
 
-        }
+            }
 
 
         } else if (xhttp.readyState == 4 && xhttp.status !== 200) {
             document.getElementById("loading").innerHTML = "Error talking with beacon. Are you logged in?";
-                throw new Error('Error talking with beacon. xhttp gave non 200 result');
+            throw new Error('Error talking with beacon. xhttp gave non 200 result');
 
         }
 
@@ -62,11 +62,11 @@ function GetJSONfromBeacon(Id,StartDate,EndDate,callback) {
 
     if (Id !== null) {
 
-    xhttp.open("GET","https://beacon.ses.nsw.gov.au/Api/v1/Jobs/Search?Q=&StartDate="+StartDate.toISOString()+"&EndDate="+EndDate.toISOString()+"&Hq="+Id+"&ViewModelType=2&PageIndex=1&PageSize=1000&SortField=Id&SortOrder=desc", true);
+        xhttp.open("GET", "https://beacon.ses.nsw.gov.au/Api/v1/Jobs/Search?Q=&StartDate=" + StartDate.toISOString() + "&EndDate=" + EndDate.toISOString() + "&Hq=" + Id + "&ViewModelType=2&PageIndex=1&PageSize=1000&SortField=Id&SortOrder=desc", true);
 
     } else {
-            xhttp.open("GET","https://beacon.ses.nsw.gov.au/Api/v1/Jobs/Search?Q=&StartDate="+StartDate.toISOString()+"&EndDate="+EndDate.toISOString()+"&ViewModelType=2&PageIndex=1&PageSize=1000&SortField=Id&SortOrder=desc", true);
+        xhttp.open("GET", "https://beacon.ses.nsw.gov.au/Api/v1/Jobs/Search?Q=&StartDate=" + StartDate.toISOString() + "&EndDate=" + EndDate.toISOString() + "&ViewModelType=2&PageIndex=1&PageSize=1000&SortField=Id&SortOrder=desc", true);
 
-    } 
+    }
     xhttp.send();
 };
