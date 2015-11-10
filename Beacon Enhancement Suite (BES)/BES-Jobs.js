e70444eefd47855feba44f5e03f91a53303d517a
Tim Dykes <ttdykes@Tims-MacBook-Pro.local>
2015-11-10 23:23:14 +1100

    refactor

--- Beacon Enhancement Suite (BES)/BES-Jobs.js
+++ Beacon Enhancement Suite (BES)/BES-Jobs.js
@@ -1,37 +1,34 @@
 //JOB Screen specific Code
-
-
 //add summary button
-
 var bar = document.getElementsByClassName("btn-group pull-left text-left");
 console.log(bar);
 
 
 var summarybutton = document.createElement("a");
-summarybutton.id="BESSummaryButton";
+summarybutton.id = "BESSummaryButton";
 summarybutton.classList.add("btn");
 summarybutton.classList.add("btn-sm");
 summarybutton.classList.add("btn-default");
 summarybutton.style.marginLeft = "20px";
 summarybutton.style.background = "blue";
 summarybutton.style.color = "white";
-summarybutton.href="#"
+summarybutton.href = "#"
 
-summarybutton.innerHTML = "<img width=\"16px\" style=\"vertical-align: top;margin-right:3px\" src=\""+chrome.extension.getURL("tv.png")+"\">Summary Screen";
+summarybutton.innerHTML = "<img width=\"16px\" style=\"vertical-align: top;margin-right:3px\" src=\"" + chrome.extension.getURL("tv.png") + "\">Summary Screen";
 bar[0].appendChild(summarybutton);
 //
 
 var statsbutton = document.createElement("a");
-statsbutton.id="BESStatsButton";
+statsbutton.id = "BESStatsButton";
 statsbutton.classList.add("btn");
 statsbutton.classList.add("btn-sm");
 statsbutton.classList.add("btn-default");
 statsbutton.style.marginLeft = "20px";
 statsbutton.style.background = "rebeccapurple";
 statsbutton.style.color = "white";
-statsbutton.href="#"
+statsbutton.href = "#"
 
-statsbutton.innerHTML = "<img width=\"16px\" style=\"vertical-align: top;margin-right:3px\" src=\""+chrome.extension.getURL("stats.png")+"\">Statistics Screen";
+statsbutton.innerHTML = "<img width=\"16px\" style=\"vertical-align: top;margin-right:3px\" src=\"" + chrome.extension.getURL("stats.png") + "\">Statistics Screen";
 bar[0].appendChild(statsbutton);
 
 
@@ -39,9 +36,9 @@ bar[0].appendChild(statsbutton);
 //set the extension code var into the head
 var summaryUrl = chrome.extension.getURL("");
 var s = document.createElement('script');
-    s.setAttribute('type', 'text/javascript');
-    s.innerHTML = "var summaryUrl = \""+summaryUrl+"\"";
-    (document.head || document.documentElement).appendChild(s)
+s.setAttribute('type', 'text/javascript');
+s.innerHTML = "var summaryUrl = \"" + summaryUrl + "\"";
+(document.head || document.documentElement).appendChild(s)
 
 
 
