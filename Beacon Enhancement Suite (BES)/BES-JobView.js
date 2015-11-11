fecf742f869c7c51779fee5437c1c3bb2c5e9479
OSPFNeighbour <ttdykes@gmail.com>
2015-11-12 10:09:56 +1100

    Don't remember what i did

--- Beacon Enhancement Suite (BES)/BES-JobView.js
+++ Beacon Enhancement Suite (BES)/BES-JobView.js
@@ -10,12 +10,11 @@ var s = document.createElement('script');
 s.src = chrome.extension.getURL('BES-JobsViewContent.js');
 (document.head || document.documentElement).appendChild(s)
 
+
+//Finalise Job Quick Text
+
 var block = document.getElementById("finaliseJobModal").getElementsByClassName("modal-body");
 var child = block[0]
-
-
-console.log(child);
-
 
 
 var div = document.createElement("div");
@@ -31,17 +30,16 @@ label.classList.add("col-md-4");
 label.classList.add("col-lg-3");
 label.classList.add("control-label");
 
-label.innerText = "BES Quick Fill";
+label.innerText = "BES Quick Text";
 
 
 var textboxdiv = document.createElement("div");
-textboxdiv.classList.add("col-md-4");
-textboxdiv.classList.add("col-lg-3");
+textboxdiv.classList.add("col-lg-9");
 
 
 var textbox = document.createElement("select");
-textbox.id = "QuickFillBox";
-textbox.width = "100%";
+textbox.classList.add("form-control");
+textbox.id = "FinaliseQuickTextBox";
 //textbox.classList.add("form-control");
 var option = document.createElement("option");
 option.text = "";
@@ -72,6 +70,66 @@ div.appendChild(innerdiv);
 
 child.insertBefore(div,child.childNodes[2]);
 
+
+
+
+
+//Finalise Job Quick Text
+
+var block = document.getElementById("completeTeamModal").getElementsByClassName("modal-body");
+var child = block[0]
+
+
+var div = document.createElement("div");
+div.classList.add("form-group");
+
+
+var innerdiv = document.createElement("div");
+innerdiv.classList.add("row");
+
+
+var label = document.createElement("label");
+label.classList.add("col-md-3");
+label.classList.add("control-label");
+
+label.innerText = "BES Quick Text";
+
+
+var textboxdiv = document.createElement("div");
+textboxdiv.classList.add("col-md-9");
+
+
+var textbox = document.createElement("select");
+textbox.classList.add("form-control");
+
+textbox.id = "CompleteQuickTextBox";
+textbox.width = "100%";
+
+var quickText = ["","NSW SES volunteers attended scene and resident no longer required assistance."]
+                    for (var i = 0; i < quickText.length; i++) {
+                        var opt = document.createElement('option');
+                        opt.text = quickText[i];
+                        opt.value = quickText[i];
+                        textbox.add(opt);
+                    }
+
+
+
+
+textboxdiv.appendChild(textbox);
+
+
+
+
+innerdiv.appendChild(label);
+
+innerdiv.appendChild(textboxdiv);
+
+div.appendChild(innerdiv);
+
+console.log(child.childNodes)
+
+child.insertBefore(div,child.childNodes[28]);
 
 
 
