bd931209163732fff6767c54fb8053bafdf22aef
OSPFNeighbour <ttdykes@gmail.com>
2015-11-13 20:11:34 +1100

    Revert "todays hot fix fixed the need to fix the close button on the situation map."
    This reverts commit 5cd479a829151669a2a579b5e42b36ab34f19625.

--- Lighthouse/lighthouse-SituationalAwarenessContent.js
+++ Lighthouse/lighthouse-SituationalAwarenessContent.js
@@ -4,8 +4,8 @@ window.addEventListener("load", pageFullyLoaded, false);
 function pageFullyLoaded(e) {
 
 //reposition the close button
-//var bar = document.getElementsByClassName("titleButton close");
-//bar[0].style.backgroundPosition = "-33px 0px";
+var bar = document.getElementsByClassName("titleButton close");
+bar[0].style.backgroundPosition = "-33px 0px";
 
 
 //hide the maximize button
