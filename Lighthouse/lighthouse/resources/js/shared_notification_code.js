fd54e67adcc42e53b411b27d3ea9bb4b2fe8eaef
OSPFNeighbour <ttdykes@gmail.com>
2015-11-24 19:10:25 +1100

    base code to pull notifications down from beacon

---
+++ Lighthouse/lighthouse/resources/js/shared_notification_code.js
@@ -0,0 +1,55 @@
+//make the call to beacon
+function GetNotificationJSONfromBeacon(Id, host, StartDate, EndDate, callback) {
+
+    console.log("GetNotificationJSONfromBeacon called with:" + Id + "," + StartDate + "," + EndDate+", "+host);
+
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            //console.log(xhttp.responseText);
+
+            try {
+                var jobs = JSON.parse(xhttp.responseText);
+            } catch (e) {
+                throw new Error('Error talking with beacon. JSON result isnt valid');
+            };
+
+            // Make sure the callback is a function​
+            if (typeof callback === "function") {
+                console.log("GetNotificationJSONfromBeacon call back with: ");
+                console.log(jobs);
+
+                callback(jobs);
+
+            }
+
+
+        } else if (xhttp.readyState == 4 && xhttp.status !== 200) {
+            throw new Error('Error talking with beacon. xhttp gave a  non 200 result. Are you logged in?');
+
+        }
+
+
+    }
+
+    if (Id !== null || typeof unit == undefined) {
+
+                if (Id.split(",").length == 1)
+                {
+                    xhttp.open("GET", "https://"+host+"/Api/v1/Notifications/Search?CreatedStartDate=" + StartDate.toISOString() + "&CreatedEndDate=" + EndDate.toISOString() + "&Hq=" + Id + "&PageIndex=1&PageSize=20000&SortField=CreatedOn&SortOrder=desc", true);
+                } else {
+                    var hqString = "";
+                    Id.split(",").forEach(function(d){
+                        hqString=hqString+"&Hq="+d
+                    });
+                    console.log(hqString)
+                    xhttp.open("GET", "https://"+host+"/Api/v1/Notifications/Search?CreatedStartDate=" + StartDate.toISOString() + "&CreatedEndDate=" + EndDate.toISOString() + hqString + "&PageIndex=1&PageSize=20000&SortField=CreatedOn&SortOrder=desc", true);
+
+                }
+    } else {
+        xhttp.open("GET", "https://"+host+"/Api/v1/Notifications/Search?CreatedStartDate=" + StartDate.toISOString() + "&CreatedEndDate=" + EndDate.toISOString() + "&PageIndex=1&PageSize=20000&SortField=CreatedOn&SortOrder=desc", true);
+
+    }
+    xhttp.send();
+};
