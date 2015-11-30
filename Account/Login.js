ca5034f89aa485a6335ed8af19c62621712e147a
OSPFNeighbour <ttdykes@gmail.com>
2015-11-30 11:24:24 +1100

    implemented Sams code into login

--- Account/Login.js
+++ Account/Login.js
@@ -5,31 +5,17 @@ s.rel="SHORTCUT ICON";
 document.getElementsByTagName('head')[0].appendChild(s);
 //
 
-
-//Add keep alive box to login screen
-var bar = document.getElementById("Password");
-
-var div = document.createElement("div");
-
-div.style.width="100%";
-div.style.height="100px";
-div.style.borderRadius = "10px";
-div.style.marginTop = "20px";
-div.style.border="1px";
-div.style.backgroundColor="grey";
-div.style.color="white";
-div.style.lineHeight="50px";
+//replace window title with job number
+var s = document.createElement('script');
+    s.setAttribute('type', 'text/javascript');
+    s.innerHTML = "document.title = \"Beacon - Login\"";
+    (document.head || document.documentElement).appendChild(s)
 
 
 
-div.innerHTML="<input autocomplete=\"off\" id=\"lighthouseKeepLogin\" type=\"checkbox\">  Try keep my session active</input><h6>If your beacon session idles for 25 minutes you will be promped to extend your session.</h6>";
+var url = chrome.extension.getURL("lighthouse128.png");
+$('#loginForm > fieldset > .row:nth-last-child(2)').before('<div class="lighthouse-keepalive"><img src="' + url + '" /><input autocomplete=\"off\" id=\"lighthouseKeepLogin\" type=\"checkbox\">  Try keep my session active</input><div class="text">If your beacon session idles for 25 minutes you will be promped to extend your session.</div></div>');
 
-var button = document.createElement("input");
-button.id="lighthouseKeepLogin";
-button.type="checkbox";
-button.innerHTML = "Remind me when I idle";
-
-bar.parentNode.insertBefore(div,bar.nextSibling);
 
 
 chrome.storage.sync.get({
@@ -50,5 +36,3 @@ chrome.storage.sync.set({
 
 });
 
-var url = chrome.extension.getURL("lighthouse128.png");
-$('#loginForm > fieldset > .row:last').prepend('<div class="something"><img src="' + url + '" /><div class="text">blergh blargh foo bar</div></div>');
