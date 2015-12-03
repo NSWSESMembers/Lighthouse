abd15a0353517069280e897ebf0fe9b310c63919
Luke Stevenson <lucanos@gmail.com>
2015-12-03 17:21:40 +1100

    CHANGE - Rewrite of the "Team Summary Button" insertion to use jQuery instead of Vanilla JS

--- Teams/Teams.js
+++ Teams/Teams.js
@@ -1,29 +1,10 @@
 console.info('Lighthouse: Teams/Teams.js');
 
 //add summary button
-var bar = document.getElementsByClassName("widget-header");
-console.log(bar);
-
-
-var summarybutton = document.createElement("a");
-summarybutton.id = "lighthouseTeamSummaryButton";
-summarybutton.classList.add("btn");
-summarybutton.classList.add("btn-sm");
-summarybutton.classList.add("btn-default");
-summarybutton.style.marginLeft = "20px";
-summarybutton.style.background = "blue";
-summarybutton.style.color = "white";
-summarybutton.href = "#"
-
-summarybutton.innerHTML = "<img width=\"16px\" style=\"vertical-align: top;margin-right:5px\" src=\"" + chrome.extension.getURL("lh.png") + "\">Summary Screen";
-bar[0].appendChild(summarybutton);
-//
-
-
-
+$lighthouseTeamSummaryButton = jQuery('<a href="#" id="lighthouseTeamSummaryButton" class="btn btn-sm btn-default" style="margin-left:20px;background:blue;color:#FFF"><img style="width:16px;vertical-align:top;margin-right:5px" src="' + chrome.extension.getURL('lh.png') + '">Summary Screen</a>');
+jQuery('.widget-header').append($lighthouseTeamSummaryButton);
 
 //inject our JS resource
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('/Teams/content/Teams.js');
 (document.head || document.documentElement).appendChild(s)
-
