2976ffc8636ad8378e483798c6bd5dd6307fd88d
OSPFNeighbour <ttdykes@gmail.com>
2015-12-04 12:38:15 +1100

    Paged JSON pulls
    JSON Search calls pull in page sets.

--- lighthouse/resources/js/shared_job_code.js
+++ lighthouse/resources/js/shared_job_code.js
@@ -1,59 +1,46 @@
 //make the call to beacon
 function GetJSONfromBeacon(unit, host, StartDate, EndDate, callback) {
 
-    var limit = 5000;
-
-    console.log("GetJSONfromBeacon called with:" + StartDate + "," + EndDate+", "+host);
-    console.log(unit);
-    console.log("telling the keep alive system we are still active")
-    chrome.runtime.sendMessage({activity: true}, function(response) {console.log(response)});
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            //console.log(xhttp.responseText);
-
-            try {
-                var jobs = JSON.parse(xhttp.responseText);
-                jobs.Results.length == limit && alert("That was a big request! the maximum number of jobs you can return is "+limit+". try selecting a smaller time period");
-            } catch (e) {
-                document.getElementById("loading").innerHTML = "Error talking with beacon. Are you logged in?";
-                throw new Error('Error talking with beacon. JSON result isnt valid');
-            };
-
-            // Make sure the callback is a function​
-            if (typeof callback === "function") {
-                console.log("GetJSONfromBeacon call back with: ");
-                console.log(jobs);
-
-                callback(jobs);
-
-            }
+    var url = "";
+    console.log("GetJSONfromBeacon called with:" + StartDate + "," + EndDate + ", " + host);
+    console.log("telling the keep alive system we are still active");
+    chrome.runtime.sendMessage({
+        activity: true
+    }, function(response) {
+        console.log(response)
+    });
 
 
-        } else if (xhttp.readyState == 4 && xhttp.status !== 200) {
-            throw new Error('Error talking with beacon. xhttp gave a  non 200 result. Are you logged in?');
-
-        }
-
-
-    }
     if (unit !== null || typeof unit == undefined) {
-        if (Array.isArray(unit) == false)
-        {
-            xhttp.open("GET", "https://"+host+"/Api/v1/Jobs/Search?StartDate=" + StartDate.toISOString() + "&EndDate=" + EndDate.toISOString() + "&Hq=" + unit.Id + "&ViewModelType=6&PageIndex=1&PageSize="+limit+"&SortField=Id&SortOrder=desc", true);
+        if (Array.isArray(unit) == false) {
+            url = "https://" + host + "/Api/v1/Jobs/Search?StartDate=" + StartDate.toISOString() + "&EndDate=" + EndDate.toISOString() + "&Hq=" + unit.Id + "&ViewModelType=6&SortField=Id&SortOrder=desc";
         } else {
             var hqString = "";
-            unit.forEach(function(d){
-                hqString=hqString+"&Hq="+d.Id
+            unit.forEach(function(d) {
+                hqString = hqString + "&Hq=" + d.Id
             });
             console.log(hqString)
-            xhttp.open("GET", "https://"+host+"/Api/v1/Jobs/Search?StartDate=" + StartDate.toISOString() + "&EndDate=" + EndDate.toISOString() + hqString + "&ViewModelType=6&PageIndex=1&PageSize="+limit+"&SortField=Id&SortOrder=desc", true);
+            url = "https://" + host + "/Api/v1/Jobs/Search?StartDate=" + StartDate.toISOString() + "&EndDate=" + EndDate.toISOString() + hqString + "&ViewModelType=6&SortField=Id&SortOrder=desc";
 
         }
     } else {
-        xhttp.open("GET", "https://"+host+"/Api/v1/Jobs/Search?StartDate=" + StartDate.toISOString() + "&EndDate=" + EndDate.toISOString() + "&ViewModelType=6&PageIndex=1&PageSize="+limit+"&SortField=Id&SortOrder=desc", true);
+        url = "https://" + host + "/Api/v1/Jobs/Search?StartDate=" + StartDate.toISOString() + "&EndDate=" + EndDate.toISOString() + "&ViewModelType=6&SortField=Id&SortOrder=desc";
 
     }
-    xhttp.send();
-};
+
+    goGetMeSomeJSONFromBeacon(url, function(results) { //call for the JSON, rebuild the array and return it when done.
+
+        console.log("GetJSONfromBeacon call back with: ");
+        var obj = {
+            "Results": results
+        }
+        console.log(obj)
+        callback(obj);
+
+    })
+
+}
+
+
+
+
---
+++ lighthouse/resources/js/shared_json_code.js
@@ -0,0 +1,42 @@
+function goGetMeSomeJSONFromBeacon(url, cb) { //take url and a page limit and loop until a result returns less than we asked for
+        const perPageLimit = 5; //small for testing
+        var currentPage = 0;
+        var totalResults = [];
+        goGet(HandleResults); //make the first call to kick off the loop
+        function HandleResults(result) { //check the length of the return
+            console.log("Results Returned :" + result.Results.length)
+            if (result.Results.length == perPageLimit) //number of results is the limit, must be more to get.
+            {
+                totalResults.push.apply(totalResults, result.Results);
+                goGet(HandleResults);
+
+            } else {
+                console.log("Got them all");
+                totalResults.push.apply(totalResults, result.Results);
+                cb(totalResults); //we are done
+            }
+        }
+        function goGet(cb) { //make the XMLHttpRequest with the given URL
+            var xhttp = new XMLHttpRequest();
+            currentPage++;
+            console.log("Fetching page #" + currentPage)
+            xhttp.onreadystatechange = function() {
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                    //console.log(xhttp.responseText);
+
+                    try {
+                        console.log("Page #"+currentPage+" is back");
+                        var result = JSON.parse(xhttp.responseText);
+                        cb(result);
+                    } catch (e) {
+                        throw new Error('Error talking with beacon. JSON result isnt valid');
+                    };
+                }
+            }
+            console.log("fetching page #"+currentPage);
+            xhttp.open("GET", url + "&PageIndex=" + currentPage + "&PageSize=" + perPageLimit, true);
+            xhttp.send();
+
+        }
+
+    }
