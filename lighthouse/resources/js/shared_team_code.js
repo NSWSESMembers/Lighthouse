93fdf6d58acd57da89ccaee35083d4eff0c45dac
OSPFNeighbour <ttdykes@gmail.com>
2015-12-04 12:38:15 +1100

    Paged JSON pulls
    JSON Search calls pull in page sets.

--- lighthouse/resources/js/shared_team_code.js
+++ lighthouse/resources/js/shared_team_code.js
@@ -29,54 +29,38 @@ function GetJSONTeamsfromBeacon(unit, host, StartDate, EndDate, callback) {
 
     console.log("GetJSONTeamsfromBeacon called with:" + unit + "," + host+", "+StartDate + "," + EndDate);
 
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            //console.log(xhttp.responseText);
-
-            try {
-                var teams = JSON.parse(xhttp.responseText);
-            } catch (e) {
-                document.getElementById("loading").innerHTML = "Error talking with beacon. Are you logged in?";
-                throw new Error('Error talking with beacon. JSON result isnt valid');
-            };
-
-            // Make sure the callback is a function​
-            if (typeof callback === "function") {
-                console.log("GetJSONTeamsfromBeacon call back with: ");
-                console.log(teams);
-
-                callback(teams);
-
-            }
-
-
-        } else if (xhttp.readyState == 4 && xhttp.status !== 200) {
-            throw new Error('Error talking with beacon. xhttp gave a  non 200 result. Are you logged in?');
-
-        }
-
-
-    }
+    var url = ""
+    
 
     if (unit !== null || typeof unit == undefined) {
 
                 if (Array.isArray(unit) == false)
                 {
-                    xhttp.open("GET", "https://"+host+"/Api/v1/Teams/Search?StatusStartDate=" + StartDate.toISOString() + "&StatusEndDate=" + EndDate.toISOString() + "&AssignedToId=" + unit.Id + "&CreatedAtId=" + unit.Id + "&PageIndex=1&PageSize=20000&SortField=CreatedOn&SortOrder=desc", true);
+                    url = "https://"+host+"/Api/v1/Teams/Search?StatusStartDate=" + StartDate.toISOString() + "&StatusEndDate=" + EndDate.toISOString() + "&AssignedToId=" + unit.Id + "&CreatedAtId=" + unit.Id + "&SortField=CreatedOn&SortOrder=desc";
                 } else {
                     var hqString = "";
                     unit.forEach(function(d){
                         hqString=hqString+"&AssignedToId="+d.Id+"&CreatedAtId="+d.Id
                     });
                     console.log(hqString)
-                    xhttp.open("GET", "https://"+host+"/Api/v1/Teams/Search?StatusStartDate=" + StartDate.toISOString() + "&StatusEndDate=" + EndDate.toISOString() + hqString + "&PageIndex=1&PageSize=20000&SortField=CreatedOn&SortOrder=desc", true);
+                    url =  "https://"+host+"/Api/v1/Teams/Search?StatusStartDate=" + StartDate.toISOString() + "&StatusEndDate=" + EndDate.toISOString() + hqString + "&SortField=CreatedOn&SortOrder=desc";
 
                 }
     } else {
-        xhttp.open("GET", "https://"+host+"/Api/v1/Teams/Search?StatusStartDate=" + StartDate.toISOString() + "&StatusEndDate=" + EndDate.toISOString() + "&PageIndex=1&PageSize=20000&SortField=CreatedOn&SortOrder=desc", true);
+        url = "https://"+host+"/Api/v1/Teams/Search?StatusStartDate=" + StartDate.toISOString() + "&StatusEndDate=" + EndDate.toISOString() + "&SortField=CreatedOn&SortOrder=desc";
 
     }
-    xhttp.send();
+    
+    goGetMeSomeJSONFromBeacon(url, function(results) { //call for the JSON, rebuild the array and return it when done.
+
+        console.log("GetJSONfromBeacon call back with: ");
+        var obj = {
+            "Results": results
+        }
+        console.log(obj)
+        callback(obj);
+
+    })
+
+
 };
