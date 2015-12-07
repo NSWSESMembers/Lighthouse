1b8220156ed7d15fc6c40f1ad9a14d315e109d2d
OSPFNeighbour <ttdykes@gmail.com>
2015-12-07 21:48:22 +1100

    moved page var to parent function to keep fetch function just working on getting the requested page back

--- lighthouse/resources/js/shared_json_code.js
+++ lighthouse/resources/js/shared_json_code.js
@@ -1,8 +1,9 @@
 function goGetMeSomeJSONFromBeacon(url, progresscb, cb) { //take url and a page limit and loop until a result returns less than we asked for
         const perPageLimit = 300; //small for testing
-        var currentPage = 0;
+        var currentPage = 1;
         var totalResults = [];
-        goGet(HandleResults); //make the first call to kick off the loop
+        goGet(HandleResults,currentPage); //make the first call to kick off the loop
+
         function HandleResults(result) { //check the length of the return
             console.log("Results Returned :" + result.Results.length)
             console.log(result);
@@ -11,8 +12,8 @@ function goGetMeSomeJSONFromBeacon(url, progresscb, cb) { //take url and a page 
                 totalResults.push.apply(totalResults, result.Results);
                 progresscb(totalResults.length,result.TotalItems)
                 console.log("Total collected:"+totalResults.length);
-                goGet(HandleResults);
-
+                currentPage++; //guess we should get the next
+                goGet(HandleResults,currentPage);
             } else {
                 console.log("Got them all");
                 totalResults.push.apply(totalResults, result.Results);
@@ -21,15 +22,13 @@ function goGetMeSomeJSONFromBeacon(url, progresscb, cb) { //take url and a page 
                 cb(totalResults); //we are done
             }
         }
-        function goGet(cb) { //make the XMLHttpRequest with the given URL
+        function goGet(cb,page) { //make the XMLHttpRequest with the given URL
             var xhttp = new XMLHttpRequest();
-            currentPage++;
             xhttp.onreadystatechange = function() {
                 if (xhttp.readyState == 4 && xhttp.status == 200) {
                     //console.log(xhttp.responseText);
-
                     try {
-                        console.log("Page #"+currentPage+" is back");
+                        console.log("Page #"+page+" is back");
                         var result = JSON.parse(xhttp.responseText);
                         cb(result);
                     } catch (e) {
@@ -42,8 +41,8 @@ function goGetMeSomeJSONFromBeacon(url, progresscb, cb) { //take url and a page 
                     progresscb(-1,-1)
                 }
             }
-            console.log("fetching page #"+currentPage);
-            xhttp.open("GET", url + "&PageIndex=" + currentPage + "&PageSize=" + perPageLimit, true);
+            console.log("fetching page #"+page);
+            xhttp.open("GET", url + "&PageIndex=" + page + "&PageSize=" + perPageLimit, true);
             xhttp.send();
 
         }
