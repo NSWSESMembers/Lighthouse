56d17bd7da5849ed8b6f7ccbf0f37bfbfcac70c7
OSPFNeighbour <ttdykes@gmail.com>
2015-12-23 11:59:32 +1100

    footer is hidden until page is rendered

--- pages/scripts/stats.js
+++ pages/scripts/stats.js
@@ -24,6 +24,8 @@ function removeStopwords(string) {
   var length = words.length;
   for (var i = 0; i < length; i++) {
     var word = words[i].trim();
+    var word = word.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+
     if(word != "" && stopwords.indexOf(word) == -1) {
       filteredWords.push(word);
     }
@@ -49,6 +51,7 @@ $(function() {
     onClose:function(){
      $('#loading').hide();
      $('#results').show();
+     $('footer').show();
    }
  });
 
