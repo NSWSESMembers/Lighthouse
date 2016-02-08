d35b06cd52a1e20fb31e632c309bef805df12a27
OSPFNeighbour <ttdykes@gmail.com>
2016-02-08 11:30:07 +1100

    fix #51. add alternating row colors to table. set text length on sit on scene

--- src/injectscripts/jobs/view.js
+++ src/injectscripts/jobs/view.js
@@ -191,21 +191,26 @@ function checkAddressHistory(){
           var jobAddress_StreetNumber_Max = parseInt( jobAddress_StreetNumber_tmp[2] , 10 );
           var jobAddress_StreetNumber_Min = parseInt( jobAddress_StreetNumber_tmp[( typeof jobAddress_StreetNumber_tmp[1] == 'undefined' ? 2 : 1 )] , 10 );
           $.each( response.responseJSON.Results , function( k , v ){
-              console.log( k , v );
-              if( v.Id == jobId ){
-                console.log( 'Current Job Match' );
+            console.log( k , v );
+            if( v.Id == jobId ){
+              console.log( 'Current Job Match' );
+              return true;
+            }
+            //No street number so its the same road
+              if (v.Address.StreetNumber == null) {
+                history_rows.street.push( checkAddressHistory_constructRow( v, false, k) );
                 return true;
               }
               // Construct Row
               if( v.Address.PrettyAddress == address.PrettyAddress ){
                 console.log( 'Perfect Address Match' );
-                history_rows.exact.push( checkAddressHistory_constructRow( v , true ) );
+                history_rows.exact.push( checkAddressHistory_constructRow( v , true, k) );
                 return true;
               }
               // Not an exact address match, so include the address in the result row
               if( v.Address.StreetNumber.replace(/\D+/g,'') == address.StreetNumber.replace(/\D+/g,'') ){
                 console.log( 'Partial Address Match - Possible Townhouses/Apartments' );
-                history_rows.partial.push( checkAddressHistory_constructRow( v ) );
+                history_rows.partial.push( checkAddressHistory_constructRow( v, false, k) );
                 return true;
               }
               var rowAddress_StreetNumber_tmp = re_StreetNumber_Parts.exec( v.Address.StreetNumber );
@@ -213,77 +218,77 @@ function checkAddressHistory(){
               var rowAddress_StreetNumber_Min = parseInt( rowAddress_StreetNumber_tmp[( typeof rowAddress_StreetNumber_tmp[1] == 'undefined' ? 2 : 1 )] , 10 );
               if( Math.abs( jobAddress_StreetNumber_Min - rowAddress_StreetNumber_Max ) == 2 || Math.abs( jobAddress_StreetNumber_Max - rowAddress_StreetNumber_Min ) == 2 ){
                 console.log( 'Immediate Neighour' );
-                history_rows.neighbour.push( checkAddressHistory_constructRow( v ) );
+                history_rows.neighbour.push( checkAddressHistory_constructRow( v, false, k) );
                 return true;
               }
               console.log( 'Street Address Match' );
-              history_rows.street.push( checkAddressHistory_constructRow( v ) );
+              history_rows.street.push( checkAddressHistory_constructRow( v, false, k) );
             });
-          content = '<table>'+
-          '<thead>'+
-          '<tr><th>Job #</th><th>Address</th><th rowspan="2">Details</th></tr>'+
-          '<tr><th>Status</th><th>Date</th></tr>'+
-          '</thead>'+
-          '<tbody>';
-          $.each(history_rows,function(k,v){
-            var section_heading = false;
-            var section_always = false;
-            switch(k){
-              case 'exact' :
-              section_heading = 'Exact Address';
-              section_always = true;
-              break;
-              case 'partial' :
-              section_heading = 'Townhouse, Apartment or Battleaxe';
-              break;
-              case 'neighbour' :
-              section_heading = 'Neighbouring Address';
-              break;
-              case 'street' :
-              section_heading = 'Same Street';
-              break;
-            }
-            if( ( v.length || section_always ) && section_heading ){
-              content += '<tr class="job-view-history-section job-view-history-'+k+'"><th colspan="3">'+section_heading+'</th></tr>';
-            }
-            if( v.length ){
-              content += v.join('');
-            }else if( section_always ){
-              content += '<tr class="job-view-history-none"><td colspan="3"><em>No Previous Reports</em></td></tr>';
-            }
-          });
-          content += '</tbody>'+
-          '</table>';
-        }else{
-          console.log( 'Address Search - No History' );
-        }
-        break;
-        case 'error' :
-        content = '<em>Unable to Perform Address Search</em>';
-        break;
-        case 'nocontent' :
-        content = '<em>Address Search returned Unexpected Data</em>';
-        break;
-        case 'timeout' :
-        content = '<em>Address Search Timed Out</em>';
-        break;
-        case 'abort' :
-        content = '<em>Address Search Aborted</em>';
-        break;
-        case 'parsererror' :
-        content = '<em>Address Search returned Unexpected Data</em>';
-        break;
-      }
+content = '<table>'+
+'<thead>'+
+'<tr><th>Job #</th><th>Address</th><th rowspan="2">Details</th></tr>'+
+'<tr><th>Status</th><th>Date</th></tr>'+
+'</thead>'+
+'<tbody>';
+$.each(history_rows,function(k,v){
+  var section_heading = false;
+  var section_always = false;
+  switch(k){
+    case 'exact' :
+    section_heading = 'Exact Address';
+    section_always = true;
+    break;
+    case 'partial' :
+    section_heading = 'Townhouse, Apartment or Battleaxe';
+    break;
+    case 'neighbour' :
+    section_heading = 'Neighbouring Address';
+    break;
+    case 'street' :
+    section_heading = 'Same Street';
+    break;
+  }
+  if( ( v.length || section_always ) && section_heading ){
+    content += '<tr class="job-view-history-section job-view-history-'+k+'"><th colspan="3">'+section_heading+'</th></tr>';
+  }
+  if( v.length ){
+    content += v.join('');
+  }else if( section_always ){
+    content += '<tr class="job-view-history-none"><td colspan="3"><em>No Previous Reports</em></td></tr>';
+  }
+});
+content += '</tbody>'+
+'</table>';
+}else{
+  console.log( 'Address Search - No History' );
+}
+break;
+case 'error' :
+content = '<em>Unable to Perform Address Search</em>';
+break;
+case 'nocontent' :
+content = '<em>Address Search returned Unexpected Data</em>';
+break;
+case 'timeout' :
+content = '<em>Address Search Timed Out</em>';
+break;
+case 'abort' :
+content = '<em>Address Search Aborted</em>';
+break;
+case 'parsererror' :
+content = '<em>Address Search returned Unexpected Data</em>';
+break;
+}
       // Insert content into DOM element
       $('#job_view_history div.form-group')
-        .html(content);
+      .html(content);
 
     }
   });
 }
 setTimeout( checkAddressHistory , 1000 );
 
-function checkAddressHistory_constructRow( v , sameAddress ){
+function checkAddressHistory_constructRow( v , sameAddress, oddEvenCount ){
 
   var options = {
     weekday: "short",
@@ -293,15 +298,28 @@ function checkAddressHistory_constructRow( v , sameAddress ){
     hour12: false
   };
 
-  var thedate = new Date(new Date(v.JobReceived).getTime() + (new Date(v.JobReceived).getTimezoneOffset() * 60000));
-  var address = ( typeof sameAddress == 'undefined' ? v.Address.StreetNumber+' '+v.Address.Street : 'Same Address' );
+  //Trim job ID
 
-  return '<tr class="job-view-history job-view-history-status-'+v.JobStatusType.Name.toLowerCase()+'">'+
+  var RowisEven = !!(oddEvenCount && !(oddEvenCount%2))
+  if (RowisEven) {RowColor = 'evenRow'} else {RowColor = 'oddRow'};
+
+
+v.Address.Street = v.Address.Street.replace("ROAD","RD");
+v.Address.Street = v.Address.Street.replace("STREET","ST");
+v.Address.Street = v.Address.Street.replace("AVENUE","AVE");
+
+
+
+
+  var thedate = new Date(new Date(v.JobReceived).getTime() + (new Date(v.JobReceived).getTimezoneOffset() * 60000));
+  var address = ( v.Address.StreetNumber != null ? (sameAddress == false ? v.Address.StreetNumber+' '+v.Address.Street : 'Same Address' ) : v.Address.Street );
+
+  return '<tr class="job-view-history '+RowColor+' job-view-history-status-'+v.JobStatusType.Name.toLowerCase()+'">'+
   '<th><a href="/Jobs/'+v.Id+'">'+v.Identifier+'</a></th>'+
   '<td>'+address+'</td>'+
-  '<td rowspan="2" valign="top">'+( v.SituationOnScene == null ? 'View job for more detail' : v.SituationOnScene )+'</td>'+
+  '<td rowspan="2" valign="top">'+( v.SituationOnScene == null ? 'View job for more detail' : ( v.SituationOnScene.length > 80 ? v.SituationOnScene.substring(0,78)+"..." : v.SituationOnScene ) )+'</td>'+
   '</tr>'+
-  '<tr class="job-view-history job-view-history-status-'+v.JobStatusType.Name.toLowerCase()+'">'+
+  '<tr class="job-view-history '+RowColor+' job-view-history-status-'+v.JobStatusType.Name.toLowerCase()+'">'+
   '<td>'+v.JobStatusType.Name+'</td>'+
   '<td title="'+thedate.toLocaleTimeString("en-au", options)+'">'+moment(thedate).fromNow()+'</td>'+
   '</tr>';
