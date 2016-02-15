3e62da477e2a99531c67fbdfef05a661a8ee6f28
OSPFNeighbour <ttdykes@gmail.com>
2016-02-15 16:50:56 +1100

    old/older

--- src/injectscripts/jobs/view.js
+++ src/injectscripts/jobs/view.js
@@ -352,7 +352,7 @@ function checkAddressHistory(){
             );
 
             // Show/Hide Handler
-            var $job_view_history_toggle_old = $( <div class="job_view_history_toggle_old"><span>Show</span> old Jobs</div> )
+            var $job_view_history_toggle_old = $( <div class="job_view_history_toggle_old"><span>Show</span> Older Jobs</div> )
               .click(function() {
                 var $t = $(this);
                 var $p = $t.closest('fieldset.job_view_history_group');
