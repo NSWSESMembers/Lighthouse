b7d3cbcee286ed57845eeb4b57441e27caddc365
OSPFNeighbour <ttdykes@gmail.com>
2016-02-19 12:03:29 +1100

    url hash is now preserved on resource call

--- src/contentscripts/messages/create.js
+++ src/contentscripts/messages/create.js
@@ -15,21 +15,34 @@ xhttp.onreadystatechange = function() {
 		inject('messages/create.js');
 	}
 };
-xhttp.open("GET", "https://beacon.ses.nsw.gov.au/js/messages/create#", true);
+
+function findMessageUrl(cb) {
+$('script').each(function(){
+	if(this.src.indexOf("/js/messages/create?v=") != -1){
+		cb(this.src.replace(/^[^\?]+\??/,''));
+	}
+});
+}
+
+findMessageUrl(function(url){
+console.log("Fetching https://beacon.ses.nsw.gov.au/js/messages/create#?"+url);
+xhttp.open("GET", "https://beacon.ses.nsw.gov.au/js/messages/create#?"+url, true);
 xhttp.send();
+})
 
 
 
-function renderBox() {
+
+function renderCheckBox() {
 	var selected = (localStorage.getItem("LighthouseMessagesEnabled") == "true") ? "fa-check-square-o" : "fa-square-o";
 	return (
 		<span class="pull-right h6">
-			<span id="lighthouseEnabled" class={"fa fa-lg "+selected}></span> 
-			<img style="width:16px;vertical-align:top;margin-right:5px;margin-left:5px"
-             src={chrome.extension.getURL("icons/lh-black.png")} /> Prefill With LHQ & Default Recipients
+		<span id="lighthouseEnabled" class={"fa fa-lg "+selected}></span> 
+		<img style="width:16px;vertical-align:top;margin-right:5px;margin-left:5px"
+		src={chrome.extension.getURL("icons/lh-black.png")} /> Prefill With LHQ & Default Recipients
 		</span>
 		);
 }
 
 
-$('#content div.row div.col-md-10.col-lg-9 div fieldset:nth-child(1) legend').append(renderBox);
+$('#content div.row div.col-md-10.col-lg-9 div fieldset:nth-child(1) legend').append(renderCheckBox);
