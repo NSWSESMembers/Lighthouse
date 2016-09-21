1e235de7ac974fb2bc8d8ea399d5fc3c2f95c549
Luke Stevenson <lucanos@gmail.com>
2016-09-21 23:13:39 +1000

    Modifies the Job Register Button creation and population functions. Adds Hardcopy Export button.

--- src/contentscripts/jobs/jobs.js
+++ src/contentscripts/jobs/jobs.js
@@ -5,12 +5,15 @@ var DOM = require('jsx-dom-factory');
 // Add buttons to top of job screen for summary, statistics and advanced export
 var buttonBar = $('.job-page .job-reg-widget .btn-group.pull-left.text-left');
 
-function makeButton(id, background, border, text) {
+function makeButton(id, page, background, border, text) {
   return $(
     <a href="#"
+       data-page={page}
        id={id}
        class="btn btn-sm btn-default"
-       style={'margin-left: 20px; background: ' + background + '; border-color: ' + border + '; color: white;'}>
+       style={'margin-left: 20px; background: ' + background + '; border-color: ' + border + '; color: white;'}
+       title={'[Lighthouse] ' + text + ' (Filtered)'}
+       target="_blank">
       <img style="width: 16px; vertical-align: top; margin-right: 5px"
            src={chrome.extension.getURL("icons/lh.png")} />{text}
     </a>
@@ -18,9 +21,10 @@ function makeButton(id, background, border, text) {
   .appendTo(buttonBar);
 }
 
-makeButton("lighthouseSummaryButton", "#175781", "#0f3a57", "Summary (Filtered)");
-makeButton("lighthouseStatsButton", "rebeccapurple", "#4c2673", "Statistics (Filtered)");
-makeButton("lighthouseExportButton", "#d2322d", "#edadab", "Advanced Export (Filtered)");
+makeButton("lighthouseSummaryButton",  'summary',        "#175781",       "#0f3a57", "Summary");
+makeButton("lighthouseStatsButton",    'stats',          "rebeccapurple", "#4c2673", "Statistics");
+makeButton("lighthouseExportButton",   'advexport',      "#d2322d",       "#edadab", "Advanced Export");
+makeButton("lighthouseHardcopyButton", 'exporthardcopy', "#175781",       "#0f3a57", "Export Hardcopy");
 
 //inject our JS resource
 inject('jobs/jobs.js');
