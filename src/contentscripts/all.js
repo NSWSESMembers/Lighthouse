e526a039aa66e2a7c0eeb01d452397be24ccd879
OSPFNeighbour <ttdykes@gmail.com>
2016-09-22 10:19:39 +1000

    removed the keep alive backend and fronted code.

--- src/contentscripts/all.js
+++ src/contentscripts/all.js
@@ -8,24 +8,6 @@ inject('all.js');
 // inject all.css - browserify-css takes care of this
 require('../styles/all.css');
 
-// notify keep alive system that a new page has been loaded (and therefore the
-// user's session has been refreshed)
-console.log("telling the keep alive system we are still active")
-chrome.runtime.sendMessage({activity: true});
-
-// notify keep alive system whenever we click on something. We let the event
-// propagate because we don't want to interfere with regular operation of <a>
-$('a').click(function(e) {
-  chrome.runtime.sendMessage({activity: true, link: this},function(){});
-});
-// notify keepalive system 
-$(window).focus(function(){
-  console.log('Focus');
-  chrome.runtime.sendMessage({focus: true},function(){});
-});
-
-
-
 //set the extension code var into the head
 var s = document.createElement('script');
 s.setAttribute('type', 'text/javascript');
