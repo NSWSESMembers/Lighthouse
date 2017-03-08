178615444f6ce873bcdebe063d816604fe9a1870
OSPFNeighbour <ttdykes@gmail.com>
2017-03-08 12:09:54 +1100

    words

--- src/background.js
+++ src/background.js
@@ -166,7 +166,7 @@ function processResult(result){
 		break
 		case 1: //positive/found
 		console.log( 'On the Register' );
-		cb(inAddressObject.PrettyAddress+" Is On Loose Fill Asbestos Register<i class='fa fa-external-link' aria-hidden='true' style='margin-left:5px;margin-right:-5px'></i>","red",true,formAddress)
+		cb(inAddressObject.PrettyAddress+" Is On The Loose Fill Asbestos Register<i class='fa fa-external-link' aria-hidden='true' style='margin-left:5px;margin-right:-5px'></i>","red",true,formAddress)
 		break
 		case 2: //negative/not found
 		console.log( 'Not the Register' );
