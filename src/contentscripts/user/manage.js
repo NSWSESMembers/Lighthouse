0abacb32de25d0e60d0ffc90cbb1061ba8d1b4d1
OSPFNeighbour <ttdykes@gmail.com>
2017-06-04 09:06:51 +1000

    fixed weird windows race condition

--- src/contentscripts/user/manage.js
+++ src/contentscripts/user/manage.js
@@ -9,6 +9,5 @@ var buttonBar = $('div.widget div.widget-header');
 $(<span><img style="width:16px;vertical-align:middle;margin-right:3px;margin-left:5px"
 		src={chrome.extension.getURL("icons/lh-black.png")} /> Hover over an orange permission box to see a description</span>).appendTo(buttonBar)
 
-console.log(buttonBar)
 
 inject('user/manage.js');
