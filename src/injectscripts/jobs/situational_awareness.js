eed3865d33681545336625cdbeb7182c00e3c9d6
Luke Quinane <luke.quinane@gmail.com>
2017-07-08 15:30:07 +1000

    Add a class to help accessing the arcgis map.

--- src/injectscripts/jobs/situational_awareness.js
+++ src/injectscripts/jobs/situational_awareness.js
@@ -1,7 +1,66 @@
 window.addEventListener("load", pageFullyLoaded, false);
 
 function pageFullyLoaded(e) {
-  //hide the maximize button
-  var max = document.getElementsByClassName("titleButton maximize");
-  max[0].classList.add("hidden");
+    //hide the maximize button
+    var max = document.getElementsByClassName("titleButton maximize");
+    max[0].classList.add("hidden");
+}
+
+$ = require('jquery');
+
+$(document).ready(function() {
+    let map = window["map"];
+    let lighthouseMap = new LighthouseMap(map);
+    //lighthouseMap.addImageMarker(-33.896628, 151.165709, "http://m.livetraffic.rta.nsw.gov.au/Assets/img/icon_warning.png");
+    window["lighthouseMap"] = lighthouseMap;
+});
+
+// Load all the arcgis classes
+const GraphicsLayer = eval("require(\"esri/layers/GraphicsLayer\");");
+const SimpleMarkerSymbol = eval("require(\"esri/symbols/SimpleMarkerSymbol\");");
+const PictureMarkerSymbol = eval("require(\"esri/symbols/PictureMarkerSymbol\");");
+const Point = eval("require(\"esri/geometry/Point\");");
+const Graphic = eval("require(\"esri/graphic\");");
+const InfoTemplate = eval("require(\"esri/InfoTemplate\");");
+
+/**
+ * A class for helping out with map access.
+ */
+class LighthouseMap {
+
+    /**
+     * Constructs a new map.
+     *
+     * @param map the arcgis map.
+     */
+    constructor(map) {
+        this.map = map;
+
+        this.graphicsLayer = new GraphicsLayer();
+        this.graphicsLayer.id = 'customLayer';
+        this.graphicsLayer.infoTempalate = this.template;
+
+        this.map.addLayer(this.graphicsLayer);
+    }
+
+    /**
+     * Adds an image marker to the map.
+     *
+     * @param lat the latitude.
+     * @param lon the longitude.
+     * @param imageUrl the URL for the marker's image.
+     */
+    addImageMarker(lat, lon, imageUrl) {
+        let point = new Point({
+            latitude: lat,
+            longitude: lon
+        });
+
+        let marker = new PictureMarkerSymbol();
+        marker.setHeight(16);
+        marker.setWidth(16);
+        marker.setUrl(imageUrl);
+
+        this.graphicsLayer.add(new Graphic(point, marker));
+    }
 }
