20cad4bd4a7ffdbd343dfbb30ce608dc43ec106a
Luke Quinane <luke.quinane@gmail.com>
2017-07-08 22:49:43 +1000

    Add in a list of known rescue helicopters.

--- src/injectscripts/jobs/situational_awareness.js
+++ src/injectscripts/jobs/situational_awareness.js
@@ -12,6 +12,57 @@ function pageFullyLoaded(e) {
     window["lighthouseMap"] = lighthouseMap;
 }
 
+function showRescueHelicopters(lighthouseMap) {
+    //https://opensky-network.org/api/states/all
+}
+
+/**
+ * A class for rescue helicopter details
+ */
+class Helicopter {
+    /**
+     * Constructs a new helicopter.
+     *
+     * @param icao24 the ICAO-24 hex code.
+     * @param rego the registration tag, e.g. "VH-TJE".
+     * @param name the flight name, e.g. "RSCU201".
+     * @param model the model, e.g. "AW-139".
+     */
+    constructor(icao24, rego, name, model) {
+        this.icao24 = icao24;
+        this.rego = rego;
+        this.name = name;
+        this.model = model;
+    }
+}
+
+// A list of rescue helicopters
+const helicopters = [
+    // Toll Air Ambulance
+    new Helicopter('7C6178', 'VH-TJE', 'RSCU201', 'AW-139'),
+    new Helicopter('7C6179', 'VH-TJF', 'RSCU202', 'AW-139'),
+    new Helicopter('7C617A', 'VH-TJG', 'RSCU203', 'AW-139'),
+    new Helicopter('7C617B', 'VH-TJH', 'RSCU204', 'AW-139'),
+    new Helicopter('7C617C', 'VH-TJI', 'RSCU206', 'AW-139'),
+    //new Helicopter('7C617D', 'VH-TJJ', '??', 'AW-139'),
+    new Helicopter('7C617E', 'VH-TJK', 'RSCU208', 'AW-139'),
+    new Helicopter('7C6182', 'VH-TJO', 'RSCU209', 'AW-139'),
+
+    // Careflight
+    new Helicopter('7C0635', 'VH-BIF', 'CFH4', 'BK117'),
+
+    // Westpac
+    new Helicopter('7C81CC', 'VH-ZXA', 'WP1', 'AW-139'),
+    new Helicopter('7C81CD', 'VH-ZXB', 'WP2', 'AW-139'),
+    new Helicopter('7C81CE', 'VH-ZXC', 'WP3', 'AW-139'),
+    new Helicopter('7C81CF', 'VH-ZXD', 'WP4', 'AW-139'),
+
+    // PolAir
+    new Helicopter('7C4D03', 'VH-PHX', 'POLAIR1', 'EC-AS355'),
+    new Helicopter('7C4CF8', 'VH-PHM', 'POLAIR4', 'EC-135'),
+    new Helicopter('7C4D05', 'VH-PHZ', 'POLAIR5', '412EPI'),
+];
+
 // Load all the arcgis classes
 const GraphicsLayer = eval("require(\"esri/layers/GraphicsLayer\");");
 const SimpleMarkerSymbol = eval("require(\"esri/symbols/SimpleMarkerSymbol\");");
