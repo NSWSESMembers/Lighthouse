641800d4a4f4038ea396e74c1a4b352f4d3f5a4b
OSPFNeighbour <ttdykes@gmail.com>
2017-10-02 21:41:32 +1100

    tweak to tasking lighthouse menu

--- src/injectscripts/jobs/tasking.js
+++ src/injectscripts/jobs/tasking.js
@@ -3,6 +3,11 @@ const MapManager = require('../../../pages/lib/map/InjectScriptMapManager.js');
 window.addEventListener('load', pageFullyLoaded, false);
 
 function pageFullyLoaded(e) {
-    let mapManager = new MapManager();
-    mapManager.initialise(filterViewModel);
+	if (taskingViewModel.showMap.peek() == false) //dont do map stuff if the map is disabled
+	{
+		$('#lhlayers').hide();
+	} else {
+		let mapManager = new MapManager();
+		mapManager.initialise(filterViewModel);
+	}
 }
