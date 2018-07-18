05aa656fbc0c424386b7267236519e8eb418891b
OSPFNeighbour <ttdykes@gmail.com>
2018-07-18 14:15:10 +1000

    changes to asbestos code due to FT changes

--- src/contentscripts/jobs/create.js
+++ src/contentscripts/jobs/create.js
@@ -13,13 +13,8 @@ window.addEventListener("message", function(event) {
     return;
   if (event.data.type && (event.data.type == "FROM_PAGE_FTASBESTOS_SEARCH")) {
     chrome.runtime.sendMessage({type: "asbestos", address: event.data.address}, function(response) {
-      if (response.resultbool == false)
-      {
-        response.requrl = ''
-      } else {
-        window.postMessage({ type: "FROM_LH", result: true }, "*");
-      }
-      asbestosBoxColor(response.result,response.colour,response.requrl)
+     window.postMessage({ type: "FROM_LH", result: response.resultbool }, "*");
+    asbestosBoxColor(response.result,response.colour,response.requrl)
     });
   } else if (event.data.type && (event.data.type == "FROM_PAGE_SESASBESTOS_RESULT")) {
     window.postMessage({ type: "FROM_LH", result: true }, "*");
