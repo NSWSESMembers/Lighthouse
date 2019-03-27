c3139473b7bf0d9d175125cbeb8b2ebbc8849df6
OSPFNeighbour <ttdykes@gmail.com>
2019-03-28 10:32:27 +1100

    replaced async call with api function

--- pages/lib/shared_chrome_code.js
+++ pages/lib/shared_chrome_code.js
@@ -1,21 +1,4 @@
 chrome.manifest = (function() {
-
-    var manifestObject = false;
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-            manifestObject = JSON.parse(xhr.responseText);
-        }
-    };
-    xhr.open("GET", chrome.extension.getURL('/manifest.json'), false);
-
-    try {
-        xhr.send();
-    } catch(e) {
-        console.log('Couldn\'t load manifest.json');
-    }
-
-    return manifestObject;
-
+  var manifestData = chrome.runtime.getManifest();
+  return manifestData;
 })();
