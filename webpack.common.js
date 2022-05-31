7392b8575b3c457e90cbd4834e57f5dd02cdf42e
Tim Dykes <ttdykes@gmail.com>
2022-05-31 15:15:03 +1000

    css packaging fixes
    Fixes to get css includes working correctly

--- webpack.common.js
+++ webpack.common.js
@@ -43,7 +43,7 @@ module.exports = {
       },
       {
         test: /\.(scss|css)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
