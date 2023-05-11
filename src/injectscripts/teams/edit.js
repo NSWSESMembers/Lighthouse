262404e6b1036916e5a1cbbc4ee6d1f365245e43
Tim Dykes <ttdykes@gmail.com>
2023-05-11 17:05:56 +1000

    update for new teams code view models

--- src/injectscripts/teams/edit.js
+++ src/injectscripts/teams/edit.js
@@ -4,22 +4,22 @@
 
 
 //replace window title with team name if set
-var callsign = vm.callsign.peek();
+var callsign = teamViewModel.callsign.peek();
 if (typeof callsign !== 'undefined' && callsign !== null) {
   document.title = callsign;
 }
 
 //when team members change
-vm.members.subscribe(function() {
+teamViewModel.members.subscribe(function() {
   // auto set the first team member as TL
   var lead = false
-  $.each(vm.members.peek(), function(k, v) {
+  $.each(teamViewModel.members.peek(), function(k, v) {
     if (v.TeamLeader.peek()) {
       lead = true
     }
   })
-  if (!lead && vm.members.peek().length) { //we have no leader
+  if (!lead && teamViewModel.members.peek().length) { //we have no leader
     console.log("setting team leader to first member in team")
-    vm.setTeamLeader(vm.members.peek()[0])
+    teamViewModel.setTeamLeader(teamViewModel.members.peek()[0])
   }
 })
