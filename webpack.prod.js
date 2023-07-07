d052c20892f8668af5d8f9f64fd31e457cb9e123
Tim Dykes <ttdykes@gmail.com>
2023-07-07 14:21:25 +1000

    zip the dist folder to make a extension friendly file

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,15 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const ZipPlugin = require('zip-webpack-plugin');
+const manifest = require('./static/manifest.json');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [
+    new ZipPlugin({
+      path: '../build/',
+      filename: `${manifest.name} ${manifest.version}.zip`,
+    })
+  ]
 });
