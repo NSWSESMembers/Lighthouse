8757a5177af25957ba068b1e4a471526af41b1c2
Tim Dykes <ttdykes@gmail.com>
2023-08-17 09:46:14 +1000

    Fix view model call (#189)

--- src/injectscripts/teams/edit.js
+++ src/injectscripts/teams/edit.js
@@ -1,25 +1,25 @@
-/* global teamViewModel, $ */
+/* global vm, $ */
 //edit and create page.
 // background js fiddles with create page to expose same viewmodel as OutageDisplayType
 
 
 //replace window title with team name if set
-var callsign = teamViewModel.callsign.peek();
+var callsign = vm.callsign.peek();
 if (typeof callsign !== 'undefined' && callsign !== null) {
   document.title = callsign;
 }
 
 //when team members change
-teamViewModel.members.subscribe(function() {
+vm.members.subscribe(function() {
   // auto set the first team member as TL
   var lead = false
-  $.each(teamViewModel.members.peek(), function(k, v) {
+  $.each(vm.members.peek(), function(k, v) {
     if (v.TeamLeader.peek()) {
       lead = true
     }
   })
-  if (!lead && teamViewModel.members.peek().length) { //we have no leader
+  if (!lead && vm.members.peek().length) { //we have no leader
     console.log("setting team leader to first member in team")
-    teamViewModel.setTeamLeader(teamViewModel.members.peek()[0])
+    vm.setTeamLeader(vm.members.peek()[0])
   }
 })
