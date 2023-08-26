e5dae20ebca38d425b5db237192c14edc6292462
Tim Dykes <ttdykes@gmail.com>
2023-08-26 18:24:52 +1000

    Bad VM reference (#190)

--- src/injectscripts/teams/edit.js
+++ src/injectscripts/teams/edit.js
@@ -1,25 +1,25 @@
-/* global vm, $ */
+/* global teamViewModel, $ */
 //edit and create page.
 // background js fiddles with create page to expose same viewmodel as OutageDisplayType
 
 
 //replace window title with team name if set
-var callsign = vm.callsign.peek();
+var callsign = teamViewModel.callsign.peek();
 if (typeof callsign !== 'undefined' && callsign !== null) {
   document.title = callsign;
 }
 
 //when team members change
-vm.members.subscribe(function() {
+teamViewModel.members.subscribe(function() {
   // auto set the first team member as TL
   var lead = false
-  $.each(vm.members.peek(), function(k, v) {
+  $.each(teamViewModel.members.peek(), function(k, v) {
     if (v.TeamLeader.peek()) {
       lead = true
     }
   })
-  if (!lead && vm.members.peek().length) { //we have no leader
+  if (!lead && teamViewModel.members.peek().length) { //we have no leader
     console.log("setting team leader to first member in team")
-    vm.setTeamLeader(vm.members.peek()[0])
+    teamViewModel.setTeamLeader(teamViewModel.members.peek()[0])
   }
 })
