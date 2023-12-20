7bc991728d36a1ac937712956c0ad8f862ba1090
Tim Dykes <ttdykes@gmail.com>
2023-12-20 20:15:00 +1100

    disable page until refactored

--- src/injectscripts/jobs/tasking.js
+++ src/injectscripts/jobs/tasking.js
@@ -1,14 +1,14 @@
 /* global taskingViewModel, $, filterViewModel */
-const MapManager = require('../../lib/map/InjectScriptMapManager.js');
+// const MapManager = require('../../lib/map/InjectScriptMapManager.js');
 
-window.addEventListener('load', pageFullyLoaded, false);
+// window.addEventListener('load', pageFullyLoaded, false);
 
-function pageFullyLoaded(_e) {
-	if (taskingViewModel.showMap.peek() == false) //dont do map stuff if the map is disabled
-	{
-		$('#lhlayers').hide();
-	} else {
-		let mapManager = new MapManager();
-		mapManager.initialise(filterViewModel);
-	}
-}
+// function pageFullyLoaded(_e) {
+// 	if (taskingViewModel.showMap.peek() == false) //dont do map stuff if the map is disabled
+// 	{
+// 		$('#lhlayers').hide();
+// 	} else {
+// 		let mapManager = new MapManager();
+// 		mapManager.initialise(filterViewModel);
+// 	}
+// }
