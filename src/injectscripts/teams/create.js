af49d7ed3c3ce7dde48bb6d27444767f6f46e364
Tim Dykes <ttdykes@gmail.com>
2023-12-20 21:04:07 +1100

    team create and team edit vm updated

---
+++ src/injectscripts/teams/create.js
@@ -0,0 +1,25 @@
+/* global teamViewModel, $ */
+//edit and create page.
+// background js fiddles with create page to expose same viewmodel as OutageDisplayType
+
+
+//replace window title with team name if set
+var callsign = teamViewModel.callsign.peek();
+if (typeof callsign !== 'undefined' && callsign !== null) {
+  document.title = callsign;
+}
+
+//when team members change
+teamViewModel.members.subscribe(function() {
+  // auto set the first team member as TL
+  var lead = false
+  $.each(teamViewModel.members.peek(), function(k, v) {
+    if (v.TeamLeader.peek()) {
+      lead = true
+    }
+  })
+  if (!lead && teamViewModel.members.peek().length) { //we have no leader
+    console.log("setting team leader to first member in team")
+    teamViewModel.setTeamLeader(teamViewModel.members.peek()[0])
+  }
+})
