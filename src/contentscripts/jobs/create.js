5ca8c566be67c94b1bed4c570f7afe54a5de9033
Tim Dykes <ttdykes@gmail.com>
2025-04-02 01:06:18 +1100

    calculate road distance on job create screen

--- src/contentscripts/jobs/create.js
+++ src/contentscripts/jobs/create.js
@@ -3,6 +3,7 @@ var inject = require('../../lib/inject.js');
 var DOM = require('jsx-dom-factory').default;
 var $ = require('jquery');
 var vincenty = require('../../lib/vincenty.js');
+let distanceDebounceTimeout;
 
 window.addEventListener("message", function(event) {
   // We only accept messages from ourselves
@@ -12,9 +13,13 @@ window.addEventListener("message", function(event) {
     if (event.data.type && (event.data.type == "FROM_PAGE_JOBTYPE")) {
       if (event.data.jType) {
         $('#nearest-rescue-lhq-label').text(`Closest ${event.data.jType} LHQs`)
+        $('#nearest-rescue-drive-lhq-label').text(`Closest ${event.data.jType} LHQs by Road`)
+
         $('#nearest-rescue-lhq-group').show()
+        $('#nearest-rescue-drive-lhq-group').show()
     } else {
       $('#nearest-rescue-lhq-group').hide()
+      $('#nearest-rescue-drive-lhq-group').hide()
     }
     }
 
@@ -34,118 +39,178 @@ window.addEventListener("message", function(event) {
 
 
   }  else if (event.data.type && (event.data.type == "FROM_PAGE_LHQ_DISTANCE")) {
-    var t0 = performance.now('');
-    $('#nearest-lhq-text').text('Searching...')
-    $('#nearest-rescue-lhq-text').text('Searching...')
-  $.getJSON(chrome.runtime.getURL("resources/SES_HQs.geojson"), function (data) {
-    let distances = []
-    let rescueDistances = []
-    data.features.forEach(function(v){
-      v.distance = vincenty.distVincenty(v.properties.POINT_Y,v.properties.POINT_X,event.data.lat,event.data.lng)/1000
-      distances.push(v)
-      if (event.data.report != null) {
-      let unitAccreditations = event.data.report[v.properties.UNIT_CODE] 
-      if (typeof unitAccreditations != "undefined") {
-        let jobType = event.data.jType
-        let combo = []
+    if (distanceDebounceTimeout) {
+      clearTimeout(distanceDebounceTimeout);
+    }
+    distanceDebounceTimeout = setTimeout(() => {
+      var t0 = performance.now('');
+      $('#nearest-lhq-text').text('Searching...')
+      $('#nearest-rescue-lhq-text').text('Searching...')
+      $('#nearest-rescue-drive-lhq-text').text('Searching...')
+      $.getJSON(chrome.runtime.getURL("resources/SES_HQs.geojson"), function (data) {
+        let distances = []
+        let rescueDistances = []
+        data.features.forEach(function(v){
+          v.distance = vincenty.distVincenty(v.properties.POINT_Y,v.properties.POINT_X,event.data.lat,event.data.lng)/1000
+          distances.push(v)
+          if (event.data.report != null) {
+          let unitAccreditations = event.data.report[v.properties.UNIT_CODE] 
+          if (typeof unitAccreditations != "undefined") {
+            let jobType = event.data.jType
+            let combo = []
 
-        switch (jobType) {
-          case "FR":
-          if (unitAccreditations['Flood Rescue in Water'] == "Available") {
-            combo.push('In') 
-          }
-          if (unitAccreditations['Flood Rescue on Water'] == "Available") {
-            combo.push('On') 
-          }
-          if (combo.length >= 1) {
-            v.properties.QUAL = combo.join('/')
-            rescueDistances.push(v)
-          }
-          break;
-          case "RCR":
-          if (unitAccreditations['RCR'] == "Available") {
-            v.properties.QUAL = 'RCR'
-            rescueDistances.push(v)
-          }  
-          if (unitAccreditations['GLR'] == "Available") {
-            v.properties.QUAL = 'GLR'
-            rescueDistances.push(v)
-          }
-          break
-          case "VR":
-          if (unitAccreditations['Vertical Rescue'] == "Available") {
-            rescueDistances.push(v)
-          }
-          break;
-          default:
-            if (unitAccreditations[jobType] == "Available") {
-              rescueDistances.push(v)
+            switch (jobType) {
+              case "FR":
+              if (unitAccreditations['Flood Rescue in Water'] == "Available") {
+                combo.push('In') 
+              }
+              if (unitAccreditations['Flood Rescue on Water'] == "Available") {
+                combo.push('On') 
+              }
+              if (combo.length >= 1) {
+                v.properties.QUAL = combo.join('/')
+                rescueDistances.push(v)
+              }
+              break;
+              case "RCR":
+              if (unitAccreditations['RCR'] == "Available") {
+                v.properties.QUAL = 'RCR'
+                rescueDistances.push(v)
+              }  
+              if (unitAccreditations['GLR'] == "Available") {
+                v.properties.QUAL = 'GLR'
+                rescueDistances.push(v)
+              }
+              break
+              case "VR":
+              if (unitAccreditations['Vertical Rescue'] == "Available") {
+                rescueDistances.push(v)
+              }
+              break;
+              default:
+                if (unitAccreditations[jobType] == "Available") {
+                  rescueDistances.push(v)
+                }
+              break
             }
-          break
-        }
-         }
-        }
-    })
-    let _sortedDistances = distances.sort(function(a, b) {
-      return a.distance - b.distance
-    });
-
-    let _sortedRescueDistances = rescueDistances.sort(function(a, b) {
-      return a.distance - b.distance
-    });
-    
-    let nearestLhqStrings = []
-    for(let i = 0; i < 4; i++){
-      if (typeof _sortedDistances[i] != "undefined") {
-        let newDom = (
-          <a type="button" class="btn btn-default btn-sm" style="margin-bottom:5px" data-unit={_sortedDistances[i].properties.UNIT_CODE}>{_sortedDistances[i].properties.HQNAME} ({_sortedDistances[i].distance.toFixed(2)} kms)</a>
-        )
-        $(newDom).click(function(e) {
-          e.preventDefault()
-          window.postMessage({ type: "FROM_LH_SETASSIGNEDUNIT", code: _sortedDistances[i].properties.UNIT_CODE}, "*");
+             }
+            }
         })
-        nearestLhqStrings.push(newDom)
-      }
-    }
+        let _sortedDistances = distances.sort(function(a, b) {
+          return a.distance - b.distance
+        });
 
-    let nearestRescueLhqStrings = []
-    for(let i = 0; i < 4; i++){
-      if (typeof _sortedRescueDistances[i] != "undefined") {
-        let rescueString = ''
-        if (typeof _sortedRescueDistances[i].properties.QUAL != 'undefined') { //they have an extra qual string on their unit from above logic
-          rescueString = ` (${_sortedRescueDistances[i].properties.QUAL})`
+        let _sortedRescueDistances = rescueDistances.sort(function(a, b) {
+          return a.distance - b.distance
+        });
+        
+        let nearestLhqStrings = []
+        for(let i = 0; i < 4; i++){
+          if (typeof _sortedDistances[i] != "undefined") {
+            let newDom = (
+              <a type="button" class="btn btn-default btn-sm" style="margin-bottom:5px" data-unit={_sortedDistances[i].properties.UNIT_CODE}>{_sortedDistances[i].properties.HQNAME} ({_sortedDistances[i].distance.toFixed(2)} kms)</a>
+            )
+            $(newDom).click(function(e) {
+              e.preventDefault()
+              window.postMessage({ type: "FROM_LH_SETASSIGNEDUNIT", code: _sortedDistances[i].properties.UNIT_CODE}, "*");
+            })
+            nearestLhqStrings.push(newDom)
+          }
         }
-        let newDom = (
-          <a type="button" class="btn btn-info btn-sm" style="margin-bottom:5px" data-unit={_sortedRescueDistances[i].properties.UNIT_CODE}>{_sortedRescueDistances[i].properties.HQNAME}{rescueString} ({_sortedRescueDistances[i].distance.toFixed(2)} kms)</a>
-        )
-        $(newDom).click(function(e) {
-          e.preventDefault()
-          window.postMessage({ type: "FROM_LH_SETASSIGNEDUNIT", code: _sortedRescueDistances[i].properties.UNIT_CODE}, "*");
+
+        let nearestRescueLhqStrings = []
+        for(let i = 0; i < 4; i++){
+          if (typeof _sortedRescueDistances[i] != "undefined") {
+            let rescueString = ''
+            if (typeof _sortedRescueDistances[i].properties.QUAL != 'undefined') { //they have an extra qual string on their unit from above logic
+              rescueString = ` (${_sortedRescueDistances[i].properties.QUAL})`
+            }
+            let newDom = (
+              <a type="button" class="btn btn-info btn-sm" style="margin-bottom:5px" data-unit={_sortedRescueDistances[i].properties.UNIT_CODE}>{_sortedRescueDistances[i].properties.HQNAME}{rescueString} ({_sortedRescueDistances[i].distance.toFixed(2)} kms)</a>
+            )
+            $(newDom).click(function(e) {
+              e.preventDefault()
+              window.postMessage({ type: "FROM_LH_SETASSIGNEDUNIT", code: _sortedRescueDistances[i].properties.UNIT_CODE}, "*");
+            })
+          nearestRescueLhqStrings.push(newDom)
+          }
+        }
+
+        let nearestRescueDriveLhq = []
+        let promises = [];
+        let nearestRescueDriveLhqStrings = []
+        
+        //handle the drive rescue distances as an array of promises due to ajax calls to resolve them all
+            for (let i = 0; i < 9; i++) {
+              if (typeof _sortedRescueDistances[i] != "undefined") {
+                let rescueString = '';
+                if (typeof _sortedRescueDistances[i].properties.QUAL != 'undefined') {
+                  rescueString = ` (${_sortedRescueDistances[i].properties.QUAL})`;
+                }
+
+                let promise = new Promise((resolve, reject) => {
+                  $.getJSON(`https://graphhopper.lighthouse-extension.com/route?point=${_sortedRescueDistances[i].properties.POINT_Y},${_sortedRescueDistances[i].properties.POINT_X}&point=${event.data.lat},${event.data.lng}&instructions=false&type=json&key=lighthouse&ch.disable=true`, function (data) {
+              resolve({ unit: _sortedRescueDistances[i], distance: data.paths[0].distance / 1000 });
+                  }).fail((error) => reject(error));
+                });
+
+                promises.push(promise);
+              }
+            }
+
+        // Wait for all promises to resolve
+        Promise.all(promises)
+        .then((results) => {
+          nearestRescueDriveLhq.push(...results);
+          nearestRescueDriveLhq.sort((a, b) => a.distance - b.distance);
+
+        for(let i = 0; i < 4; i++){
+          if (typeof nearestRescueDriveLhq[i] != "undefined") {
+            let rescueString = ''
+            if (typeof nearestRescueDriveLhq[i].unit.properties.QUAL != 'undefined') { //they have an extra qual string on their unit from above logic
+              rescueString = ` (${nearestRescueDriveLhq[i].unit.properties.QUAL})`
+            }
+            let newDom = (
+              <a type="button" class="btn btn-warning btn-sm" style="margin-bottom:5px" data-unit={nearestRescueDriveLhq[i].unit.properties.UNIT_CODE}>{nearestRescueDriveLhq[i].unit.properties.HQNAME}{rescueString} ({nearestRescueDriveLhq[i].distance.toFixed(2)} kms)</a>
+            )
+            $(newDom).click(function(e) {
+              e.preventDefault()
+              window.postMessage({ type: "FROM_LH_SETASSIGNEDUNIT", code: _sortedRescueDistances[i].unit.properties.UNIT_CODE}, "*");
+            })
+            nearestRescueDriveLhqStrings.push(newDom)
+          }
+        }
+
+        $('#nearest-rescue-drive-lhq-text').empty()
+        $('#nearest-rescue-drive-lhq-text').append(nearestRescueDriveLhqStrings)
+
         })
-      nearestRescueLhqStrings.push(newDom)
-      }
-    }
+        .catch((error) => {
+          console.error("Error fetching rescue drive distances:", error);
+          $('#nearest-rescue-drive-lhq-text').text('Error calculating distances');
+        });
 
 
-    if (nearestLhqStrings.length == 0) {
-      nearestLhqStrings.push('No Results')
-    }
+        if (nearestLhqStrings.length == 0) {
+          nearestLhqStrings.push('No Results')
+        }
 
-    if (nearestRescueLhqStrings.length == 0) {
-      nearestRescueLhqStrings.push('No Results')
-    }
+        if (nearestRescueLhqStrings.length == 0) {
+          nearestRescueLhqStrings.push('No Results')
+        }
 
-    $('#nearest-lhq-text').empty()
-    $('#nearest-lhq-text').append(nearestLhqStrings)
+        $('#nearest-lhq-text').empty()
+        $('#nearest-lhq-text').append(nearestLhqStrings)
 
 
-    $('#nearest-rescue-lhq-text').empty()
-    $('#nearest-rescue-lhq-text').append(nearestRescueLhqStrings)
+        $('#nearest-rescue-lhq-text').empty()
+        $('#nearest-rescue-lhq-text').append(nearestRescueLhqStrings)
 
-    var t1 = performance.now();
-    console.log("Call to calculate distances from LHQs took " + (t1 - t0) + " milliseconds.")
+        var t1 = performance.now();
+        console.log("Call to calculate distances from LHQs took " + (t1 - t0) + " milliseconds.")
 
-  })
+      })
+    }, 300); // 300ms debounce delay
 }
 }, false);
 
@@ -220,8 +285,22 @@ let job_asbestos_history = (
     </div>
   );
 
+  let job_nearest_drive_rescue_lhq = (
+    <div class="form-group" id="nearest-rescue-drive-lhq-group" style="display: none;">
+    <label class="col-md-2 control-label"><img style="margin-left:-21px;width:16px;vertical-align:inherit;margin-right:5px"
+    src={chrome.runtime.getURL("icons/lh-black.png")} /><span id="nearest-rescue-drive-lhq-label">Closest Accredited LHQs by Road</span></label>
+    <div id="nearest-rescue-drive-lhq-box" class="col-md-9 col-lg-9">
+    <div class="btn-toolbar" id="nearest-rescue-drive-lhq-text" style="margin: unset; margin-left: -5px;">
+    <p class="form-control-static">Waiting For A Location</p>
+    </div>
+    </div>
+    </div>
+  );
+
 
 $('#createRfaForm > fieldset:nth-child(5) > div:nth-child(2)').after(job_asbestos_history);
+
+$('#createRfaForm > fieldset:nth-child(5) > div:nth-child(14)').after(job_nearest_drive_rescue_lhq);
 
 $('#createRfaForm > fieldset:nth-child(5) > div:nth-child(14)').after(job_nearest_rescue_lhq);
 
