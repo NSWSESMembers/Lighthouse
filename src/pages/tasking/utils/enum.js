0daadd793c89c7887d15baf057c4a34b4908a396
Tim Dykes <ttdykes@gmail.com>
2025-11-15 19:43:48 +1100

    Create enum.js

---
+++ src/pages/tasking/utils/enum.js
@@ -0,0 +1,171 @@
+export const Enum = {
+    JobViewModelType:
+    {
+        "Full": {
+            "Id": 1,
+            "Name": "Full",
+            "Description": "Full",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "Simple": {
+            "Id": 2,
+            "Name": "Simple",
+            "Description": "Simple",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "Tasking": {
+            "Id": 3,
+            "Name": "Tasking",
+            "Description": "Tasking",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "Filter": {
+            "Id": 4,
+            "Name": "Filter",
+            "Description": "Filter",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "SituationalAwareness": {
+            "Id": 5,
+            "Name": "SituationalAwareness",
+            "Description": "SituationalAwareness",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "Edit": {
+            "Id": 6,
+            "Name": "Edit",
+            "Description": "Edit",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "JobRegisterPrint": {
+            "Id": 7,
+            "Name": "JobRegisterPrint",
+            "Description": "JobRegisterPrint",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        }
+    },
+    TeamStatusType:
+    {
+        "Standby": {
+            "Id": 1,
+            "Name": "Standby",
+            "Description": "Standby",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "OnAlert": {
+            "Id": 2,
+            "Name": "OnAlert",
+            "Description": "On Alert",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "Activated": {
+            "Id": 3,
+            "Name": "Activated",
+            "Description": "Activated",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "Rest": {
+            "Id": 4,
+            "Name": "Rest",
+            "Description": "Rest",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "StoodDown": {
+            "Id": 5,
+            "Name": "StoodDown",
+            "Description": "Stood down",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        }
+    },
+    JobStatusType:
+    {
+        "New": {
+            "Id": 1,
+            "Name": "New",
+            "Description": "New",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "Active": {
+            "Id": 2,
+            "Name": "Active",
+            "Description": "Active",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "Rejected": {
+            "Id": 3,
+            "Name": "Rejected",
+            "Description": "Rejected",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "Tasked": {
+            "Id": 4,
+            "Name": "Tasked",
+            "Description": "Tasked",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "Referred": {
+            "Id": 5,
+            "Name": "Referred",
+            "Description": "Referred",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "Complete": {
+            "Id": 6,
+            "Name": "Complete",
+            "Description": "Complete",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "Cancelled": {
+            "Id": 7,
+            "Name": "Cancelled",
+            "Description": "Cancelled",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        },
+        "Finalised": {
+            "Id": 8,
+            "Name": "Finalised",
+            "Description": "Finalised",
+            "ParentId": null,
+            "GroupId": null,
+            "Colour": null
+        }
+    }
+}
