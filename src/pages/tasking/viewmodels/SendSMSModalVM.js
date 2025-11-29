95b480507b33d15004acbf2965b076859965eb5e
Tim Dykes <ttdykes@gmail.com>
2025-11-29 11:44:55 +1100

    linter fix

--- src/pages/tasking/viewmodels/SendSMSModalVM.js
+++ src/pages/tasking/viewmodels/SendSMSModalVM.js
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-this-alias */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import ko from "knockout";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function CreateRadioLogModalVM(parentVM) {
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    // eslint-disable-next-line @typescript-eslint/no-this-alias, @typescript-eslint/no-unused-vars
     const self = this;
 
 };
