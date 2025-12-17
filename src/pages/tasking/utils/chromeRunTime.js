43ced97a46db5f155097c3945f4aee24da619aa8
Tim Dykes <ttdykes@gmail.com>
2025-12-17 23:30:15 +1100

    UI updates

--- src/pages/tasking/utils/chromeRunTime.js
+++ src/pages/tasking/utils/chromeRunTime.js
@@ -3,10 +3,11 @@ import { showAlert } from '../components/windowAlert.js';
 export function openURLInBeacon(url) {
 chrome.runtime.sendMessage({ type: "tasking-openURL", url: url }, function (response) {
             if (response && response.success) {
+                showAlert(response.message || `Request ${url} opened successfully in Beacon.`, "success", 2000);
                 console.log("Job opened successfully");
             } else {
                 console.error("Failed to open job");
-                showAlert(response.message || "Failed to open job in Beacon.", "warning", 4000);
+                showAlert(response.message || `Failed to open request ${url} in Beacon.`, "warning", 4000);
             }
         });
 }
